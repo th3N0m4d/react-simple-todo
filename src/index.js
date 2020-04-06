70fea6081a518a7f86717d01a72699ca6bf31388
th3N0m4d <edielton.dantas@hotmail.com>
2020-04-06 13:09:34 +0200

    Add variants to task items

--- src/index.js
+++ src/index.js
@@ -4,18 +4,14 @@ import { render } from 'react-dom'
 import './styles.scss'
 import App from './components/App'
 
+import { variants } from './consts'
+
 const mockState = {
   tasks: [
-    { id: 0, name: 'Buy wine' },
-    { id: 1, name: 'Buy cheese', completed: true },
-    { id: 2, name: 'Prepare for interview', completed: true, author: 'Edie' },
+    { id: 0, name: 'Buy wine', variant: variants.dark },
+    { id: 1, name: 'Buy cheese', completed: true, variant: variants.warning },
+    { id: 2, name: 'Prepare for interview', completed: true, author: 'Edie', variant: variants.info },
     { id: 3, name: 'Meditate', completed: true, author: 'Edie' }
-  ],
-  labels: [
-    { id: -1, label: 'Choose one...' },
-    { id: 0, label: 'House' },
-    { id: 1, label: 'Work' },
-    { id: 2, label: 'Spirituality' }
   ]
 }
 render(
