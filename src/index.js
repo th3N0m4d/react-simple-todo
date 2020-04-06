a39b879ae52ed2bd7e85df2799cceb6bab9444df
th3N0m4d <edielton.dantas@hotmail.com>
2020-04-06 11:51:39 +0200

    Create mock store before introducing redux

--- src/index.js
+++ src/index.js
@@ -4,7 +4,21 @@ import { render } from 'react-dom'
 import './styles.scss'
 import App from './components/App'
 
+const mockState = {
+  tasks: [
+    { id: 0, name: 'Buy wine' },
+    { id: 1, name: 'Buy cheese', completed: true },
+    { id: 2, name: 'Prepare for interview', completed: true, author: 'Edie' },
+    { id: 3, name: 'Meditate', completed: true, author: 'Edie' }
+  ],
+  labels: [
+    { id: -1, label: 'Choose one...' },
+    { id: 0, label: 'House' },
+    { id: 1, label: 'Work' },
+    { id: 2, label: 'Spirituality' }
+  ]
+}
 render(
-  <App />,
+  <App {...mockState} />,
   document.getElementById('app')
 )
